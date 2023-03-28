2306fbb93e75a5cc354c04ff111c647ab35713be
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-29 00:49:24 +0600

    token.interceptor implemented

--- src/mansion-rent/src/app/interceptors/token.interceptor.ts
+++ src/mansion-rent/src/app/interceptors/token.interceptor.ts
@@ -3,16 +3,37 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private auth: AuthService, private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const myToken = this.auth.getToken();
+
+    if(myToken) {
+      request = request.clone({
+        setHeaders: {Authorization: `Bearer ${myToken}`} //Bearer "myToken"
+      })
+    }
+
+    return next.handle(request).pipe(
+      catchError((err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status == 401) {
+            alert("Token is expired, Please login again");
+            this.router.navigate(['auth/login']);
+          }
+        }
+        return throwError(() => new Error("Some other error occured"));
+      })
+    );
   }
 }
