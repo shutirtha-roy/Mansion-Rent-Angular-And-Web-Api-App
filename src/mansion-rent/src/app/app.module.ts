d674e6d95d46d90650ecb20e284b41c5ab2ea6c3
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-28 20:02:12 +0600

    Header Separated

--- src/mansion-rent/src/app/app.module.ts
+++ src/mansion-rent/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PagesComponent } from './pages/pages.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent
+    PagesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
