de861d4c951f71e96b9ffce9f8447eb04872ad6e
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-28 20:22:49 +0600

    Shared Designs Added

--- src/mansion-rent/src/app/app-routing.module.ts
+++ src/mansion-rent/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
