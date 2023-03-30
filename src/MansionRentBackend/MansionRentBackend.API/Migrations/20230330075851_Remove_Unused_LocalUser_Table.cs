ad9da8d6f79eaaea831dbe7336e8303c041287d4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-03-30 14:02:15 +0600

    Unused table migration remove added

---
+++ src/MansionRentBackend/MansionRentBackend.API/Migrations/20230330075851_Remove_Unused_LocalUser_Table.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MansionRentBackend.API.Migrations
+{
+    public partial class Remove_Unused_LocalUser_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mansions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rate = table.Column<double>(type: "float", nullable: true),
+                    Sqft = table.Column<int>(type: "int", nullable: true),
+                    Occupancy = table.Column<int>(type: "int", nullable: true),
+                    Base64Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mansions", x => x.Id);
+                });
+        }
+    }
+}
