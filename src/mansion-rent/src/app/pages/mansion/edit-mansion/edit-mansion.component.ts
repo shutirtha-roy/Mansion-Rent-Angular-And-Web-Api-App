8280d8781eebeadfb9a1d4835f6e0c477469dca9
Shutirtha Roy <shutirtha.roy1@gmail.com>
2023-04-01 14:09:29 +0600

    Reactive form implemented with validations for edit

--- src/mansion-rent/src/app/pages/mansion/edit-mansion/edit-mansion.component.ts
+++ src/mansion-rent/src/app/pages/mansion/edit-mansion/edit-mansion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscriber } from 'rxjs';
 import { MansionService } from 'src/app/services/mansion.service';
@@ -11,6 +12,10 @@ import { IMansionResult } from 'src/assets/data/IMansionApiResonse';
 })
 
 export class EditMansionComponent implements OnInit {
+  mansionForm!: FormGroup;
+  submitted: boolean = false;
+  base64codeImage: string = '';
+  
   mansion: IMansionResult = {
     id: '',
     name: '',
@@ -24,9 +29,28 @@ export class EditMansionComponent implements OnInit {
     isDeleted: false
   };  
 
-  constructor(private route: ActivatedRoute, private mansionService: MansionService, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private mansionService: MansionService, 
+    private router: Router,
+    private formBuilder: FormBuilder) { 
+
+    }
 
   ngOnInit(): void {
+    this.mansionForm = this.formBuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      details: ['', Validators.required],
+      rate: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      sqft: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      occupancy: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      base64Image: ['', Validators.required],
+      createdDate: [new Date()],
+      updatedDate: [new Date()],
+      isDeleted: [false]
+    });
+    
     this.route.paramMap.subscribe({
       next: (params) => {
         const id = params.get('id');
@@ -35,23 +59,49 @@ export class EditMansionComponent implements OnInit {
           this.mansionService.getMansion(id)
           .subscribe({
             next: (response) => {
-              
               this.mansion = Object.assign(response.result);
               console.log(this.mansion);
+              
+              this.mansionForm.setValue({
+                id: this.mansion.id,
+                name: this.mansion.name,
+                details: this.mansion.details,
+                rate: this.mansion.rate,
+                sqft: this.mansion.sqft,
+                occupancy: this.mansion.occupancy,
+                base64Image: this.mansion.base64Image,
+                createdDate: this.mansion.createdDate,
+                updatedDate: this.mansion.updatedDate,
+                isDeleted: this.mansion.isDeleted
+              });
+
+              this.base64codeImage = this.mansion.base64Image;
+              
             }
           });
         }
       }
     })
   }
+  
+  onMansionUpdate() {
+    this.submitted = true;
 
-  updateMansion() {
-    this.mansionService.updateMansion(this.mansion)
-    .subscribe({
-      next: (response) => {
-        this.router.navigate(['mansion']);
-      }
+    this.mansionForm.patchValue({
+      base64Image: this.mansion.base64Image
     });
+
+    console.log(this.mansionForm.value);
+
+    if(this.mansionForm.valid)
+    {
+      this.mansionService.updateMansion(this.mansionForm.value)
+      .subscribe({
+        next: (response) => {
+          this.router.navigate(['mansion']);
+        }
+      });     
+    }
   }
 
   getImage(event: any)
@@ -64,7 +114,9 @@ export class EditMansionComponent implements OnInit {
       this.readFile(file, subscriber);
     });
     observable.subscribe((base64Code) => {
-      this.mansion.base64Image = base64Code
+      this.base64codeImage = base64Code;
+      this.mansion.base64Image = base64Code;
+      
       console.log(base64Code);
     })
   }
